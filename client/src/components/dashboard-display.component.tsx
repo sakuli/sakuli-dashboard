cfeffb2d634dc7b270d398297d50fb21f8e30a40
Sven Hettwer <sven.hettwer@consol.de>
2020-01-08 12:32:56 +0100

    (sakuli/pink-coffee#6) extracted pageIsAvailable function

--- client/src/components/dashboard-display.component.tsx
+++ client/src/components/dashboard-display.component.tsx
@@ -7,9 +7,7 @@ import IFrameComponent from "./iframe.component";
 import { Display } from "../../../server/src/api/dashboard-config-response.interface";
 import { DashboardActionResponse } from "../../../server/src/api/dashboard-action-response.interface";
 import { reloadUrl } from "../functions/reload-url.function";
-import { sleep } from "../functions/sleep.function";
-import { checkUrl } from "../services/dashboard-backend.service";
-
+import { pageIsAvailable } from "../functions/page-is-available.function";
 
 interface DisplayProps {
     display: Display
@@ -18,18 +16,6 @@ const DashboardDisplayComponent: React.FC<DisplayProps> = (props) => {
 
     const [display, setDisplay] = useState(props.display);
     const [isLoading, setIsLoading] = useState(false);
-
-    function urlNotAvailable(newUrl: string) : Promise<boolean>{
-        return checkUrl({healthEndpoint: newUrl})
-                .then(response => response.status !== 200)
-                .catch(() => true);
-    }
-
-    async function pageIsAvailable(newUrl: string, pollingInterval: number): Promise<void> {
-        while (await urlNotAvailable(newUrl)) {
-            await sleep(pollingInterval);
-        }
-    }
 
     function handleResponse(resp: DashboardActionResponse){
         const newUrl = resp.url || reloadUrl(display.url);
