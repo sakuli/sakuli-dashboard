56b9463eb150a60b21c0364ee5676d484437a978
Sven Hettwer <sven.hettwer@consol.de>
2020-01-08 13:43:14 +0100

    (sakuli/pink-coffee#6) Added some logging to checkPod

--- server/src/service/check-pod.service.ts
+++ server/src/service/check-pod.service.ts
@@ -3,6 +3,12 @@ import HttpStatusCode from "../api/HttpStatusCode";
 
 export function checkPod(url: string): Promise<number>{
    return fetch(url)
-           .then(response => response.status)
-           .catch(() => HttpStatusCode.INTERNAL_SERVER_ERROR);
+           .then(response => {
+              console.debug(`checked pod. Returned status code: ${response.status}`);
+              return response.status
+           })
+           .catch((e) => {
+              console.log("cloud not check pod", e);
+              return HttpStatusCode.INTERNAL_SERVER_ERROR
+           });
 }
