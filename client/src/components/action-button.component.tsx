85c9d0f3c050c0b0dfd57b9c7498c50916fc72c5
Simon Hofmann <simon.hofmann@consol.de>
2020-01-15 16:21:09 +0100

    (#7) Switched to <button> for our action button, updated styles, removed no longer needed display parameter

--- client/src/components/action-button.component.tsx
+++ client/src/components/action-button.component.tsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import './Dashboard.css';
-import { DashboardActionResponse, Display } from "server";
-import button from '../static/button.png';
-import { invokeAction } from "../services/dashboard-backend.service";
 import "./action-button.component.css";
-
+import { faPlayCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 interface ActionButtonProps {
-    display: Display
     onClick: () => void
 }
-const ActionButtonComponent: React.FC<ActionButtonProps> = ({display, onClick}) => {
 
+const ActionButtonComponent: React.FC<ActionButtonProps> = ({onClick}) => {
     function renderButton() {
-        if(display.actionIdentifier){
-            return <input type={"image"}
-                          className="floating-action-button"
-                          alt={"play"}
-                          src={button}
-                          onClick={() => onClick()}/>;
-        }
-
-        return null;
+        return (
+            <button className="action-button" onClick={() => onClick()}>
+                <FontAwesomeIcon icon={faPlayCircle}/>Start!
+            </button>
+        );
     }
 
     return renderButton();
