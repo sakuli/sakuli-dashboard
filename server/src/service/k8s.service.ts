5a33792eb9a6a29ca78320ca5b8e744aa04e7b5a
Cliff Le Van <cliff.le.van@consol.de>
2020-01-16 12:14:24 +0100

    (#9) added getPodStatus and deletePod to k8sService

--- server/src/service/k8s.service.ts
+++ server/src/service/k8s.service.ts
@@ -1,11 +1,11 @@
-import { CoreV1Api } from "@kubernetes/client-node";
+import { CoreV1Api, V1Pod, V1Status } from "@kubernetes/client-node";
 import { K8sClusterConfig } from "../config/k8s-cluster.config";
 import isEmpty from "../functions/is-emtpy.function";
 import { ClusterAction } from "../config/dashboard-actions.config";
 import * as http from "http";
 import fetch from "node-fetch";
 import HttpStatusCode from "../api/HttpStatusCode";
-import { CheckPodRequest } from "../api/check-pod-request.interface";
+import { CheckPodRequest } from "..";
 
 const k8s = require('@kubernetes/client-node');
 
@@ -13,6 +13,8 @@ const clusterConfig = <K8sClusterConfig>JSON.parse(process.env.CLUSTER_CONFIG ||
 export interface K8sService {
     apply: (clusterAction: ClusterAction) => Promise<http.IncomingMessage>
     checkPod: (checkPodRequest: CheckPodRequest) => Promise<number>
+    getPodStatus: (podName: string) => Promise<V1Pod>
+    deletePod: (podName: string) => Promise<V1Status>
 }
 
 export function k8sService(): K8sService{
@@ -56,9 +58,34 @@ export function k8sService(): K8sService{
             });
     }
 
+    function getPodStatus(podName: string): Promise<V1Pod>{
+        return new Promise((resolve, reject) => {
+            createK8sClient()
+                .then(k8sApi => {
+                    return k8sApi.readNamespacedPodStatus(
+                        podName,
+                        clusterConfig.namespace
+                    )})
+                .then(({body}) => resolve(body))
+                .catch(error => reject(`Could not get pod status of ${podName}: ${JSON.stringify(error)}.`));
+        });
+    }
+
+    function deletePod(podName:string): Promise<V1Status> {
+        return new Promise((resolve, reject) => {
+            createK8sClient()
+                .then(k8sApi => {
+                    return k8sApi.deleteNamespacedPod(podName, clusterConfig.namespace)
+                })
+                .then(({body}) => resolve(body))
+                .catch(error => reject(`Could not delete pod ${podName}: ${JSON.stringify(error)}.`))
+        })
+    }
+
     return ({
         apply,
-        checkPod
+        checkPod,
+        getPodStatus,
+        deletePod
     })
 }
-
