384cdedb6eafdc7f806f9fd4def94b561ebf2cf1
Cliff Le Van <cliff.le.van@consol.de>
2020-01-17 16:38:54 +0100

    (#9) check for not running pods in executeAction

---
+++ server/src/functions/pod-is-dead.function.ts
@@ -0,0 +1,21 @@
+import { V1Pod } from "@kubernetes/client-node";
+import { k8sService } from "../service/k8s.service";
+
+export default function podIsDead(pod: V1Pod): Promise<boolean>{
+    if(!pod.metadata) {
+        return Promise.resolve(true);
+    }
+    const podName: string = pod.metadata.name || "";
+
+    return k8sService().getPodStatus(podName)
+        .then(clusterPod => {
+            if(!clusterPod.status || !clusterPod.status.phase) {
+                return true;
+            }
+            return clusterPod.status.phase !== "Running" && clusterPod.status.phase !== "Pending";
+        })
+        .catch(e => {
+            console.debug(e);
+            return true;
+        });
+}
