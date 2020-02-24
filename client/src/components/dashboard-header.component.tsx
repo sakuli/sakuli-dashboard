f63f630b37902479de6f134f381d85d202f7c0eb
Simon Hofmann <simon.hofmann@consol.de>
2020-02-24 15:26:17 +0100

    (#23) Introduced header component

---
+++ client/src/components/dashboard-header.component.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./dashboard-header.component.css";
+
+const DashboardHeaderComponent: React.FC = () => {
+
+    return (
+        <div className="header">Sakuli Dashboard</div>
+    )
+};
+export default DashboardHeaderComponent;
