21992eaa85900a246c280e6439db14fa49f3c808
Simon Hofmann <simon.hofmann@consol.de>
2020-02-24 15:26:37 +0100

    (#23) Switched to using header component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import DashboardComponent from "./components/dashboard.component";
+import DashboardHeaderComponent from "./components/dashboard-header.component";
 
 const App: React.FC = () => {
 
   return (
       <div className="App">
+        <DashboardHeaderComponent/>
         <DashboardComponent/>
       </div>
   );
