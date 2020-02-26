13e93e8550a8b9fdc62b27db233ba60e76541c43
Simon Hofmann <simon.hofmann@consol.de>
2020-02-26 10:10:52 +0100

    (#23) Introduced styled-components to dashboard-header

--- client/src/components/dashboard-header.component.tsx
+++ client/src/components/dashboard-header.component.tsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import "./dashboard-header.component.css";
+import styled from "styled-components";
 
 const DashboardHeaderComponent: React.FC = () => {
 
+    const HeaderDiv = styled.div`
+        position: sticky;
+        top: 0;
+        padding: 10px;
+        background: white;
+        border-bottom: 2px solid #aadd226b;
+        box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
+        text-align: center;
+        font-weight: bold;
+        color: #523c3c;
+        z-index: 100;
+    `;
+
     return (
-        <div className="header">Sakuli Dashboard</div>
+        <HeaderDiv>Sakuli Dashboard</HeaderDiv>
     )
 };
 export default DashboardHeaderComponent;
