f996adb8112325dd7c7af275d897ce36bcbf20c6
Simon Hofmann <simon.hofmann@consol.de>
2020-02-26 10:12:42 +0100

    (#23) Introduced styled-components to dashboard

--- client/src/components/dashboard.component.tsx
+++ client/src/components/dashboard.component.tsx
@@ -1,27 +1,35 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useState} from 'react';
 import background from '../static/bg.png';
-import './Dashboard.css';
 import DashboardDisplaysComponent from "./dashboard-displays.component";
-import { getDashboardConfig } from "../services/dashboard-backend.service";
-import { Display } from "@sakuli-dashboard/api";
+import {getDashboardConfig} from "../services/dashboard-backend.service";
+import {Display} from "@sakuli-dashboard/api";
+import styled from "styled-components";
 
 const DashboardComponent: React.FC = () => {
 
     const [displays, setDisplays] = useState<Display[]>([]);
+
+    const PlaceHolderDiv = styled.div`
+        margin-top: 200px
+    `;
+
+    const DashboardHeading = styled.h1`
+        font-size: 70pt;
+    `;
 
     useEffect(() => {
         getDashboardConfig()
             .then(d => setDisplays(d.displays))
     }, []);
 
-    if(displays){
-        return <DashboardDisplaysComponent displays={displays}/>;
-    }else{
+    if (displays) {
+        return <DashboardDisplaysComponent displays={displays} layout={"column"}/>;
+    } else {
         return (
-            <div className={"no-config"}>
+            <PlaceHolderDiv>
                 <img alt={"Background"} src={background}/>
-                <h1 className={"dashboard"}>DASHBOARD</h1>
-            </div>);
+                <DashboardHeading>DASHBOARD</DashboardHeading>
+            </PlaceHolderDiv>);
     }
 };
 export default DashboardComponent;
