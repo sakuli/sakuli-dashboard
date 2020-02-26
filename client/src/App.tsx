7d4c8b1dc1782b727ae4e24245e9e16896e10f43
Simon Hofmann <simon.hofmann@consol.de>
2020-02-26 10:11:22 +0100

    (#23) Introduced styled-components to App

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,41 @@
-import React from 'react';
-import './App.css';
+import React, {useState} from 'react';
 import DashboardComponent from "./components/dashboard.component";
 import DashboardHeaderComponent from "./components/dashboard-header.component";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faThLarge, faThList} from "@fortawesome/free-solid-svg-icons";
+import styled from "styled-components";
+
+const LayoutContext = React.createContext('row');
 
 const App: React.FC = () => {
 
-  return (
-      <div className="App">
-        <DashboardHeaderComponent/>
-        <DashboardComponent/>
-      </div>
-  );
+    const [isRowLayout, setRowLayout] = useState(true);
+
+    const viewModeButton = (isRowLayout: boolean) => {
+        return (
+            <div className="buttonGroup">
+                <button>
+                    <FontAwesomeIcon icon={faThLarge}/>
+                </button>
+                <button>
+                    <FontAwesomeIcon icon={faThList}/>
+                </button>
+            </div>
+        )
+    };
+
+    const AppDiv = styled.div`
+        text-align: center;
+        background: #f2f2f2;
+        height: 100%;
+    `;
+
+    return (
+        <AppDiv>
+            <DashboardHeaderComponent/>
+            <DashboardComponent/>
+        </AppDiv>
+    );
 };
 
 export default App;
