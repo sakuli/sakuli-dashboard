84403cc894d1945b4efd0682327a3a6dc1dc0cea
Sven Hettwer <sven.hettwer@consol.de>
2020-06-09 11:45:11 +0200

    Corrected message contract, type guard and added test cases

--- api/src/interfaces/dashboard-action-response.interface.ts
+++ api/src/interfaces/dashboard-action-response.interface.ts
@@ -1,8 +1,16 @@
-import { BackendError, DisplayUpdate } from ".";
+import { DisplayUpdate } from ".";
 
 export interface DashboardActionResponse extends DisplayUpdate{
 }
 
-export function isDashboardActionResponse(json: DashboardActionResponse | BackendError) : json is DashboardActionResponse {
-  return !!((json as DashboardActionResponse).pollingInterval || (json as DashboardActionResponse).url);
+export function isDashboardActionResponse(json: any) : json is DashboardActionResponse {
+
+  function containsDashboardActionResponseFields() {
+    return !!((json as DashboardActionResponse).pollingInterval || (json as DashboardActionResponse).url);
+  }
+  function isEmptyObject() {
+    return Object.keys(json).length === 0 && json.constructor === Object;
+  }
+
+  return containsDashboardActionResponseFields() || isEmptyObject();
 }
