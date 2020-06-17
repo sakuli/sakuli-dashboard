faa220b2aa3b58a33dde8c6c5afc0b2d4e493d00
Van Cliff Le <cliff.le.van@consol.de>
2020-06-17 13:46:20 +0200

    (#40) added localstorage to ensure stable layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,16 @@
-import React, {useState} from 'react';
+import React from 'react';
 import DashboardComponent from "./components/dashboard.component";
 import {useLocale} from "./hooks/use-locale";
 import Container from 'react-bootstrap/Container';
 import DashboardHeaderComponent from "./components/dashboard-header.component";
+import {useLayout} from "./hooks/use-layout";
 
 
 export type LayoutMode = "row" | "column";
 
 const App: React.FC = () => {
 
-    const [currentLayout, setLayout] = useState<LayoutMode>("column");
-
+    const [currentLayout, setLayout] = useLayout()
     let appContainerRef: React.RefObject<HTMLDivElement> = React.createRef();
     const locale = useLocale();
 
