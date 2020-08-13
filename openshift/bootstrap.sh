dc542924c088ab97168759800a373bfcdd6720cf
MarleneSahra <marlenek@consol.de>
2020-08-13 16:11:30 +0200

    (#56) Changed deployment of the pink coffee demo to use sakuli-dashboard docker image

--- openshift/bootstrap.sh
+++ openshift/bootstrap.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 function help() {
-    echo "Usage: sh bootstrap.sh <GITHUB_SOURCE_SECRET_PATH> <CONFIG_FILE>"
+    echo "Usage: sh bootstrap.sh <DOCKER_USERNAME> <DOCKER_PASSWORD> <CONFIG_FILE>"
     echo ""
     echo "Parameters:"
-    echo "  GITHUB_SOURCE_SECRET_PATH: Path to the private key for the ssh github source secret."
+    echo "  DOCKER_USERNAME: Username for docker login"
+    echo "  DOCKER_PASSWORD: Password for docker login"
     echo "  CONFIG_FILE: Name of the config file to be loaded (located in openshift/configs)."
 }
 
@@ -15,14 +16,16 @@ if [[ ${?} != 0 ]]; then
   exit 1
 fi
 
-GITHUB_SOURCE_SECRET_PATH=${1}
-CONFIG_FILE=${2}
+DOCKER_USERNAME=${1}
+DOCKER_PASSWORD=${2}
+CONFIG_FILE=${3}
 CONFIG_FILE_PATH=${DIR}/configs/${CONFIG_FILE}
 
-[[ ! -f "${GITHUB_SOURCE_SECRET_PATH}" ]] && echo "ERROR: GITHUB_SOURCE_SECRET_PATH \"${GITHUB_SOURCE_SECRET_PATH}\" does not exist" && help && exit 1
+[[ -z "${DOCKER_USERNAME}" ]] && echo "ERROR: DOCKER_USERNAME is not defined" && help && exit 1
+[[ -z "${DOCKER_PASSWORD}" ]] && echo "ERROR: DOCKER_PASSWORD is not defined" && help && exit 1
 [[ ! -f "${CONFIG_FILE_PATH}" ]] && echo "ERROR: CONFIG_FILE \"${CONFIG_FILE_PATH}\" does not exist" && help && exit 1
 
-source ${CONFIG_FILE_PATH}
+source "${CONFIG_FILE_PATH}"
 [[ -z "${NAMESPACE}" ]] && echo "ERROR: NAMESPACE is empty" && help && exit 1
 [[ -z "${SERVICE_NAME}" ]] && echo "ERROR: SERVICE_NAME is empty" && help && exit 1
 [[ -z "${ACTION_NAMESPACE}" ]] && ACTION_NAMESPACE=${NAMESPACE}
@@ -34,24 +37,40 @@ else
   oc new-project ${NAMESPACE}
 fi
 
-GITHUB_SOURCE_SECRET="github-sakuli-dashboard"
-oc create secret generic ${GITHUB_SOURCE_SECRET} \
-    --from-file=ssh-privatekey=${GITHUB_SOURCE_SECRET_PATH} \
-    --type=kubernetes.io/ssh-auth
-
 oc create sa "${SERVICE_NAME}" -n "${ACTION_NAMESPACE}"
 oc policy add-role-to-user edit -n "${ACTION_NAMESPACE}" -z "${SERVICE_NAME}"
 LOGIN_TOKEN=$(sh $DIR/utils/get-login-token.sh "${SERVICE_NAME}" "${ACTION_NAMESPACE}")
 source ${CONFIG_FILE_PATH} #Update config with received login token
 
-oc new-app git@github.com:sakuli/sakuli-dashboard.git \
-    --name="${SERVICE_NAME}" \
-    --strategy=docker \
-    --source-secret=${GITHUB_SOURCE_SECRET} \
-    -e DASHBOARD_CONFIG="${DASHBOARD_CONFIG}" \
-    -e ACTION_CONFIG="${ACTION_CONFIG}" \
-    -e CLUSTER_CONFIG="${CLUSTER_CONFIG}" \
-    -e CRONJOB_CONFIG="${CRONJOB_CONFIG}"
+oc create secret generic sakuli-license-key \
+    --from-literal="SAKULI_LICENSE_KEY=${SAKULI_LICENSE_KEY}"
+
+oc create secret docker-registry dockerhub-sakuli-secret \
+    --docker-server=docker.io \
+    --docker-username="${DOCKER_USERNAME}" \
+    --docker-password="${DOCKER_PASSWORD}" \
+    --docker-email=unused
+
+oc secrets link builder dockerhub-sakuli-secret
+
+oc import-image sakuli-dashboard \
+    --from=docker.io/taconsol/sakuli-dashboard \
+    --confirm \
+    --scheduled=true \
+    --all=true \
+    --reference-policy=local
+#do we need reference-policy?
+
+oc process -f dashboard-template.yml \
+    -p SERVICE_NAME="${SERVICE_NAME}" \
+    -p DASHBOARD_CONFIG="${DASHBOARD_CONFIG}" \
+    -p ACTION_CONFIG="${ACTION_CONFIG}" \
+    -p CLUSTER_CONFIG="${CLUSTER_CONFIG}" \
+    -p CRONJOB_CONFIG="${CRONJOB_CONFIG}" \
+    -p NAMESPACE="${NAMESPACE}" \
+    | oc create -f -
+
+oc expose svc/${SERVICE_NAME}
 
 CREATE_ROUTE="oc create route edge ${SERVICE_NAME} --service ${SERVICE_NAME} --insecure-policy=Redirect"
 if [ -n "${DASHBOARD_HOSTNAME}" ]; then
