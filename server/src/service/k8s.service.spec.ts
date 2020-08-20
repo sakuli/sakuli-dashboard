5dc42b9ccb2712f4ceeb6f76c935ffcabb738ed3
Sven Hettwer <sven.hettwer@consol.de>
2020-08-20 13:32:03 +0200

    (#65)[WIP] should create pod test

--- server/src/service/k8s.service.spec.ts
+++ server/src/service/k8s.service.spec.ts
@@ -1,18 +1,22 @@
 jest.mock("../functions/get-configuration.function")
-jest.mock("@kubernetes/client-node")
 
 import { mockPartial } from "sneer";
 import { Configuration, getConfiguration } from "../functions/get-configuration.function";
-import { V1Pod } from "@kubernetes/client-node";
+import { CoreV1Api, V1Pod } from "@kubernetes/client-node";
+import * as http from "http";
+import { V1ObjectMeta } from "@kubernetes/client-node/dist/gen/model/v1ObjectMeta";
+import { K8sClusterConfig } from "../config/k8s-cluster.config";
 import * as K8sService from "./k8s.service";
 
 describe("k8s service", () => {
 
     beforeEach(() => {
-        jest.resetAllMocks();
+        jest.resetAllMocks()
+        jest.restoreAllMocks()
     })
 
     const getConfigurationMock = getConfiguration as any as jest.Mock<Configuration>
+    const createK8sClientSpy = jest.spyOn(K8sService, "createK8sClient");
 
     describe("apply pod config", () => {
         it("should throw if no cluster config is set", async () => {
@@ -28,6 +32,52 @@ describe("k8s service", () => {
                 //THEN
                 .rejects.toEqual(expectedRejection)
 
+        })
+
+        it("should create pod", async () => {
+
+            //GIVEN
+            const expectedResponse = mockPartial<http.IncomingMessage>({
+                statusCode: 201
+            })
+
+            const createNamespacePodMock = jest.fn().mockImplementation(async (): Promise<{
+                response: http.IncomingMessage;
+                body: V1Pod;
+            }> => {
+                return {
+                    response: expectedResponse,
+                    body: mockPartial<V1Pod>({})
+                }
+            })
+
+            createK8sClientSpy.mockImplementation(async () => {
+                    return mockPartial<CoreV1Api>({
+                        createNamespacedPod: createNamespacePodMock
+                    })
+                });
+
+            const clusterConfig = mockPartial<K8sClusterConfig>({
+                namespace: "foobar"
+            })
+            getConfigurationMock.mockImplementation(() => {
+                return mockPartial<Configuration>({
+                    k8sClusterConfig: clusterConfig
+                })
+            });
+
+            const podToCreate = mockPartial<V1Pod>({
+                metadata: mockPartial<V1ObjectMeta>({
+                    name: "Sachuli"
+                })
+            });
+
+            //WHEN
+            const applyPromise = K8sService.apply(podToCreate);
+
+            //THEN
+            await expect(applyPromise).resolves.toEqual(expectedResponse);
+            expect(createNamespacePodMock).toBeCalledWith(clusterConfig.namespace, podToCreate)
         })
     })
 })
