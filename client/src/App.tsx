2c7184c1e2fa62c6f9d40eae029624ae92532de3
Sven Hettwer <sven.hettwer@consol.de>
2021-01-15 10:28:49 +0100

    (#79) refactored App.tsx so that all dashboard components are bundled in dashboard.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,29 +1,8 @@
 import React from 'react';
 import Dashboard from "./components/dashboard";
-import { useLocale } from "./hooks/use-locale";
-import Container from 'react-bootstrap/Container';
-import DashboardHeader from "./components/dashboard-header";
-import { useLayout } from "./hooks/use-layout";
 
 const App: React.FC = () => {
-
-    const [currentLayout, setLayout] = useLayout()
-    let appContainerRef: React.RefObject<HTMLDivElement> = React.createRef();
-    const locale = useLocale();
-
-    return (
-        <div className={"vertical-scroll"} ref={appContainerRef}>
-            <DashboardHeader
-                appContainerRef={appContainerRef}
-                currentLayout={currentLayout}
-                locale={locale}
-                handleOnClick={setLayout}
-            />
-            <Container fluid={true}>
-                <Dashboard layout={currentLayout} locale={locale}/>
-            </Container>
-        </div>
-    );
+    return (<Dashboard/>);
 };
 
 export default App;
--- client/src/components/dashboard.tsx
+++ client/src/components/dashboard.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import DashboardDisplays from "./dashboard-displays";
 import { getDashboardConfig } from "../services/dashboard-backend.service";
-import { Display, LayoutMode } from "@sakuli-dashboard/api";
+import { Display } from "@sakuli-dashboard/api";
 import { BackendError, isBackendError, isDashboardConfigResponse } from "@sakuli-dashboard/api/dist";
 import DashboardPlaceholder from "./dashboard-placeholder";
+import { useLocale } from "../hooks/use-locale";
+import { useLayout } from "../hooks/use-layout";
+import DashboardHeader from "./dashboard-header";
+import Container from "react-bootstrap/Container";
 
-interface DashboardProps {
-    layout: LayoutMode;
-    locale: string;
-}
-
-const Dashboard: React.FC<DashboardProps> = (props: DashboardProps) => {
+const Dashboard: React.FC = () => {
 
     const [displays, setDisplays] = useState<Display[]>([]);
     const [backendError, setBackendError] = useState<BackendError>();
+    const [currentLayout, setLayout] = useLayout()
+    const locale = useLocale();
+    let appContainerRef: React.RefObject<HTMLDivElement> = React.createRef();
 
     useEffect(() => {
         (async function handleDashboardConfigResponse() {
@@ -26,14 +28,30 @@ const Dashboard: React.FC<DashboardProps> = (props: DashboardProps) => {
         })();
     }, []);
 
-    if (displays.length > 0) {
-        return (
-            <DashboardDisplays displays={displays} layout={props.layout} locale={props.locale}/>
-        );
-    } else {
-        return (
-            <DashboardPlaceholder backendError={backendError}/>
-        );
+    function renderDisplays(){
+        if (displays.length > 0) {
+            return (
+                <DashboardDisplays displays={displays} layout={currentLayout} locale={locale}/>
+            );
+        } else {
+            return (
+                <DashboardPlaceholder backendError={backendError}/>
+            );
+        }
     }
+
+    return(
+        <div className={"vertical-scroll"} ref={appContainerRef}>
+            <DashboardHeader
+                appContainerRef={appContainerRef}
+                currentLayout={currentLayout}
+                locale={locale}
+                handleOnClick={setLayout}
+            />
+            <Container fluid={true}>
+                {renderDisplays()}
+            </Container>
+        </div>
+    )
 };
 export default Dashboard;
