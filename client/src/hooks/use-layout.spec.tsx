d9fbf96cf73efa61e5e0bb2a86a83ad71ca2eda8
Sven Hettwer <sven.hettwer@consol.de>
2021-01-18 09:29:13 +0100

    (#79) Added use-layout.spec.tsx

---
+++ client/src/hooks/use-layout.spec.tsx
@@ -0,0 +1,47 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useLayout } from "./use-layout";
+import { act } from "react-dom/test-utils";
+
+describe("use layout", () => {
+
+    test('should return column be default', () => {
+
+        //GIVEN
+        const expectedLayout = "column"
+
+        //WHEN
+        const { result } = renderHook(() => useLayout())
+
+        //THEN
+        expect(result.current[0]).toBe(expectedLayout)
+    })
+
+    test('should return stored layout', () => {
+
+        //GIVEN
+        const expectedLayout = "row"
+        localStorage.setItem("dashboard-layout", expectedLayout)
+
+        //WHEN
+        const { result } = renderHook(() => useLayout())
+
+        //THEN
+        expect(result.current[0]).toBe(expectedLayout)
+    })
+
+    test('should set layout to storage', () => {
+
+        //GIVEN
+        const expectedLayout = "row"
+        const layoutHook1 = renderHook(() => useLayout())
+
+        //WHEN
+        act(() => {
+            layoutHook1.result.current[1](expectedLayout)
+        })
+
+        //THEN
+        const layoutHook2 = renderHook(() => useLayout())
+        expect(layoutHook2.result.current[0]).toBe(expectedLayout)
+    })
+})
--- client/src/hooks/use-layout.tsx
+++ client/src/hooks/use-layout.tsx
@@ -2,7 +2,7 @@ import { useCallback, useState } from "react";
 import { LayoutMode } from "@sakuli-dashboard/api";
 
 export const useLayout = () => {
-    const [currentLayout, setLayout] = useState<LayoutMode>((localStorage.getItem("dashboard-layout")?? "column") as LayoutMode);
+    const [currentLayout, setLayout] = useState<LayoutMode>((localStorage.getItem("dashboard-layout") ?? "column") as LayoutMode);
 
     const enhanceSetLayout = useCallback((layout: LayoutMode) => {
         setLayout(layout);
