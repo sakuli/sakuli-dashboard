e70932fb9f5ce1ee6862cfe6796cc1160d17323b
Sven Hettwer <sven.hettwer@consol.de>
2021-01-19 12:56:37 +0100

    (#79) Added dashboard placeholder test

--- client/src/components/dashboard.spec.tsx
+++ client/src/components/dashboard.spec.tsx
@@ -1,8 +1,9 @@
-import { DashboardConfigResponse } from "@sakuli-dashboard/api";
+import { BackendError, DashboardConfigResponse } from "@sakuli-dashboard/api";
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import Dashboard from "./dashboard";
 import DashboardDisplays from "./dashboard-displays";
+import DashboardPlaceholder from "./dashboard-placeholder";
 
 jest.mock("./dashboard-displays")
 
@@ -10,48 +11,48 @@ jest.mock("./dashboard-header", () => {
     return () => null
 })
 
-jest.mock("./dashboard-placeholder", () => {
-    return () => null
-})
+jest.mock("./dashboard-placeholder")
 
 
 describe("dashboard", () => {
 
-    const dashboardConfigResponse: DashboardConfigResponse = {
-        displays: [
-            {
-                actionIdentifier: "42",
-                index: 1,
-                messages: {
-                    "de": {
-                        description: "foo",
-                        infoText: "foobar"
-                    }
-                },
-                url: "http://consol.de",
-                height: "42",
-                width: "42"
-            }
-        ],
-        layout: "foobar"
-    };
-
-    beforeEach(() => {
-        jest.spyOn(global, 'fetch').mockResolvedValue(new Response(JSON.stringify(dashboardConfigResponse)));
-
-        (DashboardDisplays as jest.Mock).mockImplementation(() => {
-            return(
-                <div data-testid={"dashboard-displays"}/>
-            )
-        })
-    });
-
-    afterEach(() => {
-        jest.restoreAllMocks();
-        localStorage.removeItem("dashboard-layout")
-    });
-
     describe("display dashboards rendering", () => {
+
+        const dashboardConfigResponse: DashboardConfigResponse = {
+            displays: [
+                {
+                    actionIdentifier: "42",
+                    index: 1,
+                    messages: {
+                        "de": {
+                            description: "foo",
+                            infoText: "foobar"
+                        }
+                    },
+                    url: "http://consol.de",
+                    height: "42",
+                    width: "42"
+                }
+            ],
+            layout: "foobar"
+        };
+
+        beforeEach(() => {
+            jest.spyOn(global, 'fetch').mockResolvedValue(new Response(JSON.stringify(dashboardConfigResponse)));
+
+            (DashboardDisplays as jest.Mock).mockImplementation(() => {
+                return(
+                    <div data-testid={"dashboard-displays"}/>
+                )
+            })
+        });
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+            localStorage.removeItem("dashboard-layout")
+        });
+
+
         it("should use data from backend", async () => {
 
             //WHEN
@@ -114,6 +115,38 @@ describe("dashboard", () => {
             //THEN
             await waitFor(() => screen.getByTestId("dashboard-displays"))
             expect(DashboardDisplays).toHaveBeenLastCalledWith(expect.objectContaining({locale: expect.anything()}), expect.anything())
+        })
+    })
+
+    describe("dashboard placeholder", () => {
+
+        const backendError: BackendError = {
+            message: "foobar"
+        };
+
+        beforeEach(() => {
+            jest.spyOn(global, 'fetch').mockResolvedValue(new Response(JSON.stringify(backendError)));
+
+            (DashboardPlaceholder as jest.Mock).mockImplementation(() => {
+                return(
+                    <div data-testid={"dashboard-placeholder"}/>
+                )
+            })
+        });
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+        });
+
+        it("should render error screen", async () => {
+
+            //WHEN
+            render(<Dashboard/>);
+
+            //THEN
+            await waitFor(() => screen.getByTestId("dashboard-placeholder"))
+            expect(DashboardPlaceholder).toHaveBeenLastCalledWith({backendError}, expect.anything())
+
         })
     })
 
