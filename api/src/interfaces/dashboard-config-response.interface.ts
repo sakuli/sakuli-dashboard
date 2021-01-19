277030c8c9795995044a828620d9b5875eaa5adb
Sven Hettwer <sven.hettwer@consol.de>
2021-01-19 11:46:39 +0100

    (#79) layout information is optional

--- api/src/interfaces/dashboard-config-response.interface.ts
+++ api/src/interfaces/dashboard-config-response.interface.ts
@@ -2,7 +2,7 @@ import { Display, LayoutMode } from ".";
 
 export interface DashboardConfigResponse {
   displays: Display[]
-  layout: LayoutMode
+  layout?: LayoutMode
 }
 
 export function isDashboardConfigResponse(response: any): response is DashboardConfigResponse {
