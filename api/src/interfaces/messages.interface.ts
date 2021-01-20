4c29f7f336678a4b29d15c6fd145911772fc7209
Van Cliff Le <cliff.le.van@consol.de>
2021-01-20 10:24:23 +0100

    (#68) add validation for Messages

--- api/src/interfaces/messages.interface.ts
+++ api/src/interfaces/messages.interface.ts
@@ -2,3 +2,11 @@ export type Messages = {
     description: string
     infoText: string
 }
+
+export function isMessages(json: any): json is Messages{
+    return !!(
+      Object.keys(json).length === 2 &&
+      (json as Messages).description &&
+      (json as Messages).infoText
+    )
+}
