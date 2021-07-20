49f98a2acd627680538005b90d590a32825fa281
Sven Hettwer <sven.hettwer@consol.de>
2021-07-20 14:19:11 +0200

    (#86) Introduced displayType.interface.ts

--- api/src/interfaces/display.interface.ts
+++ api/src/interfaces/display.interface.ts
@@ -1,9 +1,11 @@
 import { isMessages, Messages } from "./messages.interface";
+import { DisplayType, isDisplayType } from "./displayType.interface";
 
 export interface Display {
     index: number
+    type?: DisplayType
     messages?: Record<string, Messages>
-    url: string
+    url?: string
     width?: string
     height?: string
     actionIdentifier?: string
@@ -39,12 +41,28 @@ export function isDisplay(json: any): json is Display {
         return !json.actionIdentifier || typeof json.actionIdentifier === "string"
     }
 
+    function validateDisplayType() {
+        if(isDisplayType(json.type) && json.type === "logs"){
+            /**
+             * If type is logs, we require an actionIdentifier to correlate
+             * the logs from the pod performing the action
+             */
+            return typeof json.actionIdentifier === "string"
+        } else{
+            /**
+             * if type is not set => default is "website"
+             * In this case we require an url to display
+             */
+            return typeof json.url === "string"
+        }
+    }
+
     if (!json) {
         return false;
     }
 
     return typeof json.index === "number" &&
-        typeof json.url === "string" &&
+        validateDisplayType() &&
         validateMessages() &&
         validateWidth()  &&
         validateHeight() &&
