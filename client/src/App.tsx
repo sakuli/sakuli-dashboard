a002147629c9816741fe67e1c1bd9d6b7a742d99
Sven Hettwer <sven.hettwer@consol.de>
2021-12-07 16:46:47 +0100

    (#88) store credentials locally

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,72 @@
 import React, { useEffect, useState } from 'react';
 import Dashboard from "./components/dashboard";
 import { getSecurityConfig, performLogin, refreshLoginInformation } from "./services/dashboard-backend.service";
-import { LoginResponse, SecurityConfigResponse, } from "@sakuli-dashboard/api";
+import { LoginResponse, } from "@sakuli-dashboard/api";
 import Login from "./components/Login";
 import { useInterval } from 'usehooks-ts'
 
 const App: React.FC = () => {
 
-    const [securityConfiguration, setSecurityConfiguration] = useState<SecurityConfigResponse>();
+    const [loggedIn, setLoggedIn] = useState<boolean>(false);
     const [loginInformation, setLoginInformation] = useState<LoginResponse>();
 
     useEffect(() => {
             (async () => {
-                setSecurityConfiguration(await getSecurityConfig())
+                const securityConfigResponse = await getSecurityConfig();
+                setLoggedIn(!securityConfigResponse.authorizationEnabled)
+                if(securityConfigResponse.authorizationEnabled){
+                    await refreshToken(getStoredLoginInformation());
+                }
             })();
         }, []);
 
+    useEffect(() => {
+        if(loginInformation){
+            localStorage.setItem("loginInformation", JSON.stringify(loginInformation));
+        }
+    }, [loginInformation]);
+
     useInterval(
-        () => {
-           refreshLoginInformation(loginInformation!.jwtRefreshToken)
-               .then(loginResponse => {
-                   if(loginResponse){
-                       setLoginInformation(loginResponse)
-                   }
-               })
-        },
+        () => {(async () => refreshToken(loginInformation))()},
         loginInformation ? loginInformation.jwtTokenTTL * 1000 / 4 : null,
     )
 
-    async function loginHandler(user: string, password: string): Promise<boolean> {
-        setLoginInformation(await performLogin(user, password))
-        return loginInformation !== undefined
+    function getStoredLoginInformation(): LoginResponse | undefined {
+        const storedLoginInformation = localStorage.getItem("loginInformation");
+        if(storedLoginInformation != null){
+            return JSON.parse(storedLoginInformation)
+        }
+        return undefined
     }
 
-    let loggedIn = function () {
-        return securityConfiguration?.authorizationEnabled !== true || loginInformation !== undefined;
-    };
+    async function refreshToken(currentLoginInformation?: LoginResponse) {
+        if(currentLoginInformation){
+            await performRefresh(currentLoginInformation);
+        }
+        else{
+            setLoggedIn(false)
+        }
+    }
+
+    async function performRefresh (currentLoginInformation: LoginResponse) {
+        const refreshedLoginInformation = await refreshLoginInformation(currentLoginInformation.jwtRefreshToken);
+        if (refreshedLoginInformation) {
+            setLoginInformation(refreshedLoginInformation)
+            setLoggedIn(true)
+        } else {
+            setLoggedIn(false)
+        }
+    }
+
+    async function loginHandler(user: string, password: string): Promise<boolean> {
+        const loginResponse = await performLogin(user, password);
+        setLoginInformation(loginResponse);
+        setLoggedIn(loginResponse !== undefined)
+        return loggedIn
+    }
 
     function renderApp() {
-        if(loggedIn()){
+        if(loggedIn){
             return <Dashboard loginInformation={loginInformation}/>;
         }else{
             return <Login performLogin={loginHandler}/>
